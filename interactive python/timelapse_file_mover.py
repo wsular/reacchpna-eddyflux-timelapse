bcbd3b4012825071fa85dde19240bdc99839ead0
Patrick O'Keeffe <pokeeffe@wsu.edu>
2013-02-26 11:44:35 -0800

    Several changes:
    - Migrate program into a main() loop
    - Correct typo in site menu
    - New functions check for necessary files
    - Update/add relevant directory paths

--- interactive python/timelapse_file_mover.py
+++ interactive python/timelapse_file_mover.py
@@ -7,13 +7,6 @@
 import os
 import subprocess
 
-antexe = r'C:\Program Files (x86)\Ant Renamer\renamer.exe'
-antarg = '-b "%s" -afr "%s" -g -x' #batch file name, source dir
-
-testloc = r'U:\ectower_data_scripts.git\CFNT timelapse rename batch process.arb'
-srcloc = r'F:\DCIM\100_WSCT'
-
-
 splashscreen = """\
 =========================================================
 =        Timelapse camera photo transfer utility        =
@@ -21,24 +14,53 @@ splashscreen = """\
 =  Regional Approaches to Climate Change | Objective 2  =
 =               Washington State University             =
 =========================================================
+"""
+srcloc = r'F:\DCIM\100_WSCT'
+antexe = r'C:\Program Files (x86)\Ant Renamer\renamer.exe'
+antarg = '-b "%s" -afr "%s" -g -x' #batch file name, source dir
+arbloc = r'..\Ant Renamer batch files\%s timelapse rename batch process.arb'
+dstloc = r'C:\SHARES\2011_REACCH\tower_%s\photos_timelapsecam'
 
-Source directory: %s
-Counting files..."""
+_codelist = {'1' : 'CFNT',
+             '2' : 'LIND',
+             '3' : 'CFCT',
+             '4' : 'MMTN' }
 
-filelist = os.listdir(srcloc)
-print splashscreen % srcloc, "%d files found" % len(filelist)
-print """
+def check_for_ant_renamer():
+    if not os.path.isfile(antexe):
+        raise IOError, 'Could not locate Ant Renamer install path'
+
+def find_batch_file(codestr):
+    arbpath = arbloc % codestr
+    if not os.path.isfile(arbpath):
+        raise IOError, 'Could not locate Ant Renamer batch files'
+    return arbpath
+
+def main():
+    print splashscreen 
+    print 'Source directory: %s \nCounting files...' % srcloc, 
+    filelist = os.listdir(srcloc)
+    print "%d files found" % len(filelist)
+    print """
 Where are these files from?
   (1) CFNT  Cook Agronomy Farm, no-till
   (2) LIND  Lind Dryland Research Station
-  (3) CFNT  Cook Agronomy Farm, conventional till
+  (3) CFCT  Cook Agronomy Farm, conventional till
   (4) MMTN  Moscow Mountain area site """
 
-choice = ''
-while not (choice.isdigit() or choice == 'q'):
-    choice = raw_input("Choose 1-4 or q to quit: ")
+    choice = ''
+    while not (choice.isdigit() or choice == 'q'):
+        choice = raw_input("Choose 1-4 or q to quit: ")
+    if choice == 'q':
+        import sys
+        sys.exit(0)
+    
+    arb = find_batch_file(_codelist[choice])
+    print 'using batch file: ', arb
 
-print 'you choose %s' % choice
+    print 'put files in: ', dstloc % _codelist[choice]
 
-raw_input('Press any key to exit...')
+    raw_input('Press any key to exit...')
 
+if __name__ == '__main__':
+    main()
