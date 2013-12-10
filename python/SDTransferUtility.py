469eecdc2993011a1239b6334cafdcae3e0a5ffc
Patrick O'Keeffe <pokeeffe@wsu.edu>
2013-12-09 18:23:03 -0800

    Implement wildcard search ability

--- python/SDTransferUtility.py
+++ python/SDTransferUtility.py
@@ -9,6 +9,8 @@ import logging
 import os
 import os.path as osp
 
+from glob import glob
+
 from Tkinter import *
 from tkMessageBox import askyesno, showerror
 from tkFileDialog import askdirectory
@@ -32,6 +34,8 @@ log.addHandler(logging.NullHandler())
 
 _prog_title_ = 'Timelapse Image Transfer Utility'
 
+_default_search_string = r'DCIM\*_WSCT\*.jpg'
+
 
 class SDTransferUtility(Frame):
     """GUI program for transferring timelapse images from SD cards"""
@@ -40,7 +44,8 @@ class SDTransferUtility(Frame):
         Frame.__init__(self, parent)
         self.pack(padx=5, pady=5, expand=YES, fill=BOTH)
 
-        self.__srcdir = StringVar()
+        self.__srch_dir = StringVar(value=SD_DRIVE) # XXX
+        self.__srch_str = StringVar(value=_default_search_string) # XXX
         self.__set_output_RO = IntVar(value=1)
         self.__set_output_A = IntVar(value=1)
 
@@ -53,49 +58,57 @@ class SDTransferUtility(Frame):
         topfrm.pack(expand=NO, fill=X, side=TOP)
         Label(topfrm, text=_prog_title_).pack(side=TOP)
 
-        srcfrm = LabelFrame(topfrm, padx=5, pady=5, relief=RIDGE,
+        sfrm = LabelFrame(topfrm, padx=5, pady=5, relief=RIDGE,
                             text='Source File Search')
-        srcfld = Entry(srcfrm, textvariable=self.__srch_dir)
-        vwsbtn = Button(srcfrm, text='View', command=self.__view_srch_dir)
-        refbtn = Button(srcfrm, text='Refresh', command=self.__refresh_src)
-        srcbtn = Button(srcfrm, text='Browse', command=self.__set_srcdir)
+        srch_lbl = Label(sfrm, text='Root search dir.:')
+        srch_fld = Entry(sfrm, textvariable=self.__srch_dir)
+        find_lbl = Label(sfrm, text='Match string:')
+        find_fld = Entry(sfrm, textvariable=self.__srch_str)
+        rfsh_btn = Button(sfrm, text='Refresh', command=self.__search)
+        brws_btn = Button(sfrm, text='Browse', command=self.__set_srch_dir)
 
-        srcfrm.pack(expand=YES, fill=X, side=TOP)
-        srcfld.pack(side=LEFT, expand=YES, fill=X, padx=5)
-        vwsbtn.pack(side=RIGHT, padx=3)
-        refbtn.pack(side=RIGHT, padx=3)
-        srcbtn.pack(side=RIGHT, padx=3)
+        sfrm.pack(expand=YES, fill=X, side=TOP)
+        srch_lbl.pack(side=LEFT)
+        srch_fld.pack(side=LEFT, expand=YES, fill=X, padx=5)
+        brws_btn.pack(side=LEFT, padx=3)
+        find_lbl.pack(side=LEFT)
+        find_fld.pack(side=LEFT, expand=YES, fill=X, padx=5)
+        rfsh_btn.pack(side=RIGHT, padx=3)
 
 
-    def __set_srcdir(self):
+
+
+    def __set_srch_dir(self):
         """Browse to source directory"""
         choice = askdirectory(title='Select source directory',
                               mustexist=True)
         if choice and osp.isdir(choice):
-            self.__srcdir.set(choice.replace('/','\\'))
-            refresh = askyesno(title='Refresh source directory',
-                           message='Source directory has changed. Refresh?')
-            if refresh:
-                self.__refresh_src()
+            self.__srch_dir.set(choice.replace('/','\\'))
+#            refresh = askyesno(title='Refresh source directory',
+#                           message='Source directory has changed. Refresh?')
+#            if refresh:
+#                self.__refresh()
 
 
-    def __refresh_src(self):
+    def __search(self):
         """Search for files in source directory"""
+        globstr = osp.join(self.__srch_dir.get(), self.__srch_str.get())
+        found = glob(globstr)
+        for fname in found:
+            print fname
 
-        print 'refreshing'
 
-
-    def __view_srcdir(self):
-        """Open source directory in file explorer"""
-        source = self.__srcdir.get()
-        if not source:
-            return False
-        if not osp.isdir(source):
-            showerror(title='View error',
-                  message=('Error occurred, could not open specified '
-                           'directory: %s') % source)
-        else:
-            os.startfile(osp.abspath(source))
+#    def __view_srcdir(self):
+#        """Open source directory in file explorer"""
+#        source = self.__srcdir.get()
+#        if not source:
+#            return False
+#        if not osp.isdir(source):
+#            showerror(title='View error',
+#                  message=('Error occurred, could not open specified '
+#                           'directory: %s') % source)
+#        else:
+#            os.startfile(osp.abspath(source))
 
     #def _
 
